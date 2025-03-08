0f6994a814de6136831cc0385abf3089d6ca87b3
edbadiola <u000681426@byui.edu>
2025-03-08 15:24:00 +0800

    update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //part 1&2
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is your guess? ");
+        // int guess = int.Parse(Console.ReadLine());
+
+        // if (magicNumber > guess)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else if (magicNumber < guess)
+        // {
+        //     Console.WriteLine("Lower");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("You guessed it!");
+        // }
+
+        //part 3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
+
+
+        
 }
